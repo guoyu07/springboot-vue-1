143c2be732c77f04ea683508a53a297f419ba8f3
jiangchong <54271523@qq.com>
2017-08-28 11:29:38 +0800

    Init Commit

--- src/main/java/com/example/model/Article.java
+++ src/main/java/com/example/model/Article.java
@@ -1,10 +1,24 @@
 package com.example.model;
 
+import java.util.Date;
+
 /**
  * 文章
+ *
  * @author jiangchong
  * @since 2017/8/28 10:03
  */
 public class Article {
+    private String id;
+    private String title;
+    private String author;
+    private ArticleType articleType;
+    private String tag;
+    private Category category;
+    private String content;
+    private Date createTime;
+    private Date modifyTime;
+    private Boolean drafts;
+    private Boolean recycle;
 
 }
--- src/main/java/com/example/model/Category.java
+++ src/main/java/com/example/model/Category.java
@@ -5,5 +5,39 @@ package com.example.model;
  * @since 2017/8/28 10:05
  */
 public class Category {
+    private Integer id;
+    private String name;
 
+    public Category() {
+
+    }
+
+    public Category(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
